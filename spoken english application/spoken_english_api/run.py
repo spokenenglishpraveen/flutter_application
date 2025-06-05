23d08613f57050fac2d2557ca7ec9f79ffa8d8f2
spokenenglishpraveen <praveen.spoken.english1@gmail.com>
2025-06-05 09:22:07 +0530

    Update run.py

--- spoken english application/spoken_english_api/run.py
+++ spoken english application/spoken_english_api/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, Blueprint
 from flask_cors import CORS
 import random
-from app.main import app  # or wherever your FastAPI or Flask app is
+
 
 
 practice_bp = Blueprint('practice', __name__)
