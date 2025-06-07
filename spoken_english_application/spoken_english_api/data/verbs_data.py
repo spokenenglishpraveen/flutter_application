f9c5166ec2c8a5b5abfac70048fc936cb07d625c
praveen <praveen@praveens-MacBook-Air.local>
2025-06-07 12:31:09 +0530

    adde

---
+++ spoken_english_application/spoken_english_api/data/verbs_data.py
@@ -0,0 +1,7 @@
+# data/verbs_data.py
+
+verbs = [
+    {"english": "analyze", "telugu": "విశ్లేషించు"},
+    {"english": "appreciate", "telugu": "అభినందించు"},
+    {"english": "anticipate", "telugu": "అంచనా వేయు"}
+]
