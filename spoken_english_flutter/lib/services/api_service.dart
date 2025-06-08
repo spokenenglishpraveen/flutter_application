f2a97307b082677dbbbb9e8629cce50aadd37c6e
praveen <praveen@praveens-MacBook-Air.local>
2025-06-08 12:36:21 +0530

    Force rebuild to fix verbs_dict import

--- spoken_english_flutter/lib/services/api_service.dart
+++ spoken_english_flutter/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'https://flutter-application-2.onrender.com';
+  static const String baseUrl = 'flutterapplication-production.up.railway.app';
 
   // --- VERBS ---
 
