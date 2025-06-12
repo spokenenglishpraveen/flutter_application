4a4f5630aa3cfde9749bca198c6383ecac1e37a6
praveen <praveen@praveens-MacBook-Air.local>
2025-06-12 20:56:45 +0530

    rqrr

--- spoken_english_api/main.py
+++ spoken_english_api/main.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 import os
 import random
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
-from spoken_english_api.app.models.user import User  # ✅ Ensure correct model path
+from spoken_english_api.app.models.user import User
+
+  # ✅ Ensure correct model pat
 
 # Load environment variables
 load_dotenv()
